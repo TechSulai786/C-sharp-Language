6dded791e542ff1a4368a116d8e6be63f074a2fd
TechSulai786 <techsulai786@outlook.com>
2023-12-01 22:52:09 +0530

    updated

---
+++ Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿public class GenericsClass<T>
+{
+    public T Data { get; set; }
+
+    public void DisplayName()
+    {
+        Console.WriteLine($"Data is: {Data}");
+    }
+}
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        GenericsClass<int> generics = new GenericsClass<int>();
+        generics.Data = 45;
+        generics.DisplayName();
+
+        GenericsClass<string> strgenerics = new GenericsClass<string>();
+        strgenerics.Data = "string.Empty";
+        strgenerics.DisplayName();
+    }
+}
---
+++ List_C#/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace List_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<string> fruits = new List<string>();
+            fruits.Add("Apple");
+            fruits.Add("Banana");
+            fruits.Add("Orange");
+
+            Console.WriteLine($"Number of fruits: {fruits.Count}");
+            fruits.Remove("Banana");
+            Console.WriteLine($"Number of fruits: {fruits.Count}");
+            foreach (string i in fruits)
+            {
+                Console.WriteLine(i);
+            }
+            fruits.Clear();
+            Console.WriteLine("List after Clear: " + string.Join(", ", fruits));
+            
+        }
+    }
+}
